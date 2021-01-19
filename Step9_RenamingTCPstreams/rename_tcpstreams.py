24c89432576ed643f7723a3cad9d294c7174ae41
hirahamed <75620121+hirahamed@users.noreply.github.com>
2021-01-19 11:41:04 +0500

    uploaded rename_tcpstreams.py

---
+++ Step9_RenamingTCPstreams/rename_tcpstreams.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+with open('assembla_data') as f:
+  data = json.load(f)
+#
+
+files_list = os.listdir()
+for f in files_list:
+    name_split = f.split("_")
+    print(name_split[0])
+    for key,value in data.items():
+        
+        # print(value['Ticket_number'])
+        if str(name_split[0]) == str(value['Ticket_number']) :
+            app_name = key.replace(" ","")
+            ending = "_".join(name_split[1:])
+            output_file = app_name+"_"+str(ending)
+            print(output_file)
+            os.rename(f, "C:\\Users\\hira ahmed\\Desktop\\Signature Verification\\Drop4-Jan\\Renamed_TCPstreams_90Apps\\"+output_file)
+            print("moving...")
+            
+
+    
